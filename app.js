67dd6f9bd27879f60d48147434d2c9e222b57cc3
Itay4 <31018228+Itay4@users.noreply.github.com>
2018-09-14 04:01:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -135,3 +135,29 @@ function callSendAPI(sender_psid, response) {
     }
   }); 
 }
+
+app.get('/', function(req, res){
+  let request_body = {
+    "recipient": {
+      "id": "1438658626236625"
+    },
+    "message": {
+    "text": "TEST"
+    }
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+    res.send('Hello World!');
+});
